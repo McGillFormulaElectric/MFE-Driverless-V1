ec2873c5091df6031f1c066c01eae586b63bdf7a
LaurenS419 <laurenspee@gmail.com>
2025-10-25 06:40:00 +0000

    FIX: move remappings to sim node

--- ros2/src/mfe_eufs_sim/launch/mfe_eufs_sim.launch.py
+++ ros2/src/mfe_eufs_sim/launch/mfe_eufs_sim.launch.py
@@ -9,5 +9,17 @@ def generate_launch_description():
             executable='bridge_node',
             name='mfe_eufs_sim',
             output='screen',
+            remappings=[
+                ('pcl/input', '/lidar/pcl/raw'),  # raw lidar data from eufs sim
+            ],
+        ),
+
+        # static transform publisher for lidar
+        Node(
+            package='tf2_ros',
+            executable='static_transform_publisher',
+            name='lidar_tf_pub',
+            arguments=['0', '0', '0', '0', '0', '0', '1', 'map', 'velodyne'],
+            output='screen'
         ),
     ])
